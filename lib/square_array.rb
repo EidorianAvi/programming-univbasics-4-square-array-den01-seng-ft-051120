78480b962cd87ad15e428430ef82d149c295841c
Rene Avila <reneavila1993@gmail.com>
2020-05-07 18:33:37 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,18 @@
+# def square_array(array)
+#   new_array = []
+#   i = 0 
+#   while i < array.length do 
+#   new_array.push(array[i]**2)
+#   i += 1
+# end
+# return new_array
+# end
+
 def square_array(array)
   new_array = []
-  i = 0 
-  while i < array.length do 
-  new_array.push(array[i]**2)
-  i += 1
-end
-return new_array
+  index = 0 
+  while index < array.length do
+    new_array << array[index]**2
+  end
+  new_array
 end
